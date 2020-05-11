79ef9002f1516f559599fa63a86447eb2c134b5b
Mirko Garozzo <mirkogarozzo@hotmail.it>
2020-05-10 23:17:06 -0500

    Added grid style

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,17 +4,22 @@ import './Card.css'
 const Card = ({ data }) => {
     const { id, address, homeowner, photoURL, price } = data;
     return (
-        <div className="card" >
-            <div className="card-body">
+        <div className={'media'}>
+            <div className={'media__img'}>
                 <img
                     alt={id}
                     src={photoURL}
                 />
             </div>
-            <div className="card-footer">
-                <p>{address}</p>
-                <p>{price}</p>
-                <p>{homeowner}</p>
+            <div className={'media-body'}>
+                <h3 className={'media__title'}>{address}</h3>
+                <p className={'media__meta'}>${price}</p>
+                <p className={'media__info'}>Sold by: {homeowner}</p>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+                <span className={'favourites'}>Save to my favourites</span>
             </div>
         </div>
     )
